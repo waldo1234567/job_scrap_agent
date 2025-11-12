f6164f5f7347bf0ff79887c895d3b250996f5c2a
waldo1234567 <waldowalerian@gmail.com>
2025-11-12 16:43:46 +0800

    workflow issue

--- yilingsi_scraper.py
+++ yilingsi_scraper.py
@@ -3,13 +3,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 import time
 import json
 from datetime import datetime
-import traceback
 import os
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 
